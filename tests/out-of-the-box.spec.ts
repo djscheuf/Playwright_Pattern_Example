883aabc6568a2b9ee36dc91f271aa2efd131d06f
Daniel Scheufler <djscheufler@gmail.com>
2024-05-01 09:14:32 -0500

    test(add multiple todos)

--- tests/out-of-the-box.spec.ts
+++ tests/out-of-the-box.spec.ts
@@ -21,6 +21,22 @@ test.describe('New Todo - Refactored',()=>{
     await _context.Then_Input_Cleared_For_Next_Entry();
     await _context.Then_Total_ToDos_In_LocalStorage_Should_Be(1);
   })
+
+  test('Add Many ToDos', async ({page})=> {
+    const _context = new OutOfTheBoxContext(page);
+
+    const exampleTodoCount = TODO_ITEMS.length;
+    for(let i=0; i++; i<exampleTodoCount-1){
+      await _context.Given_A_Todo_of(TODO_ITEMS[i])
+    }
+    await _context.Given_A_Draft_Todo_Of(TODO_ITEMS[TODO_ITEMS.length-1]);
+
+    await _context.When_Add_Todo();
+
+    await _context.Then_Todos_Should_Contain(TODO_ITEMS);
+    await _context.Then_Input_Cleared_For_Next_Entry();
+    await _context.Then_Total_ToDos_In_LocalStorage_Should_Be(exampleTodoCount);
+  })
 });
 
 test.describe('New Todo', () => {
